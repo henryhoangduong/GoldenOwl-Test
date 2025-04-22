1fb85705f27889a3f390c83be74888433d63e269
henryhoangduong <henryhoangduong@gmail.com>
2025-04-22 22:30:04 +0700

    disable any rule in eslint

--- backend/src/controllers/result.controller.ts
+++ backend/src/controllers/result.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 import { getResultByStudentSBDService } from '../services/result.service'
 import { HTTPSTATUS } from '../config/http.config'
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 export const getResultByStudentSBDController = async (req: Request, res: Response): Promise<any> => {
   const result = await getResultByStudentSBDService(req.params.sbd)
   return res.status(HTTPSTATUS.OK).json(result)
