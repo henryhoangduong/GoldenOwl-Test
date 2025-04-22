b79acf1e3690a4adff7ae2ea7d821a73cc97a463
henryhoangduong <henryhoangduong@gmail.com>
2025-04-22 09:40:31 +0700

    add script and start server

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -1,8 +1,9 @@
 import { getEnv } from '../utils/get-env'
+import 'dotenv/config'
 
 const appConfig = () => ({
   NODE_ENV: getEnv('NODE_ENV', 'development'),
-  PORT: getEnv('PORT', '5000'),
+  PORT: getEnv('PORT', '5000')
 })
 
 export const config = appConfig()
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,13 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import { config } from './config/app.config'
 const app = express()
-app.listen(config.PORT, async () => {})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.get('/', async (req: Request, res: Response) => {
+  res.json({ message: 'hello world' })
+})
+
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`)
+})
